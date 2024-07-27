86b087552a212d9b0ab297c24f454b6bd2994684
trung713 <namtrung713@gmail.com>
2024-07-27 23:16:10 +0700

    Update import streamlit as st.py

--- import streamlit as st.py
+++ import streamlit as st.py
@@ -1,11 +1,24 @@
 import streamlit as st
-st.write('chao ban')
-st.title('ti le tot nghiep')
-name = st.text_input('bai cuoi ki')
-st.pyplot()
 import pandas as pd
-df = pd.read_csv('graduation_rate.csv')({
-    'firt column' : [1,2,3,4],
-    'second column': [10,20,30,40]
+import matplotlib.pyplot as plt
+
+st.write('Chào bạn!')
+st.title('Tỷ lệ tốt nghiệp')
+
+# Text input
+name = st.text_input('Bài cuối kỳ')
+
+# Load and display DataFrame
+df = pd.DataFrame({
+    'First column': [1, 2, 3, 4],
+    'Second column': [10, 20, 30, 40]
 })
-st.write('graduation_rate.csv',df)
+
+st.write('Graduation rate DataFrame:')
+st.write(df)
+
+# Example plot
+fig, ax = plt.subplots()
+ax.plot(df['First column'], df['Second column'])
+st.pyplot(fig)
+
